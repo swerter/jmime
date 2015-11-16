11820a95d3acd0631634686e91e844ace9befbc1
Dejan Strbac <dejan.strbac@gmail.com>
2015-11-16 22:08:32 +0100

    update

--- tools/jmime_search_mailbox.c
+++ tools/jmime_search_mailbox.c
@@ -10,9 +10,11 @@ int main(int argc, char *argv[]) {
   }
 
   jmime_init();
+
   gchar **results = jmime_search_mailbox(argv[1], argv[2], 1000);
   if (results) {
     int i = 0;
+
     while (results[i])
       g_printf("%s\n", results[i++]);
 
